04382c6d81aa006e8becff1bb9a841b9756b8907
Dhairya <23bcs031@nith.ac.in>
2025-10-30 19:26:14 +0530

    Final

--- output.c
+++ output.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    int x = 5;
-if (x < 10) {
-    printf("%d\n", (int)(x));
+    int i = 1;
+while (i < 11) {
+    printf("%d\n", (int)(i * 2));
+    i = i + 1;
 }
+    float j = 1;
     return 0;
 }
---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# --- Compile and run Flex & Bison project automatically ---
+
+echo "Cleaning old files..."
+rm -f lex.yy.c parser.tab.c parser.tab.h parser output.c
+
+echo "Running Flex and Bison..."
+flex lexer.l
+bison -d parser.y
+
+echo "Compiling..."
+gcc parser.tab.c lex.yy.c -o parser
+
+echo "Generating the C program"
+./parser < test.tiny
+
+echo "Running the program..."
+gcc output.c -o output
+./output
+
